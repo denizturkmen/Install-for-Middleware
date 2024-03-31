99d9e134c8d70df619361aa6d8bce8624a4c7cc9
deniz.turkmen <turkmen_deniz@hotmail.com>
2024-03-31 09:53:17 +0300

    initial

---
+++ HashiCorp Vault/1- Install/vault-ubuntu-install.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+
+sudo apt-get update
+
+# Update the package manager and install GPG and wget
+sudo apt update && sudo apt install gpg wget
+
+# Download the keyring
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Verify the keyring
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+
+# Add the HashiCorp repository.
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Install
+sudo apt update && sudo apt install vault
